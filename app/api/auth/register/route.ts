d052d64ed85bc52d43af756570720933ec15054b
Amar Krishna <amarkrishna25@gmail.com>
2024-06-26 19:53:15 +0530

    modify register route

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -1,18 +1,27 @@
 import { prisma } from "@/utils/db";
 import { hashPassword } from "@/app/helpers/authHelpers";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(request: Request) {
-  const res = await request.json();
+export const POST = async (request: NextRequest) => {
+  const { name, email, password } = await request.json();
 
-  const hashedPassword = await hashPassword(res.password);
+  const hashedPassword = await hashPassword(password);
 
-  const dbRes = await prisma.user.create({
-    data: {
-      email: res.email,
-      password: hashedPassword,
-      name: res.name,
-    },
+  try {
+    const dbRes = await prisma.user.create({
+      data: {
+        email: email,
+        password: hashedPassword,
+        name: name,
+      },
+    });
+  } catch (err: any) {
+    return new NextResponse(err.mesage, {
+      status: 500,
+    });
+  }
+
+  return new NextResponse("User has been created", {
+    status: 201,
   });
-
-  return Response.json({ dbRes });
-}
+};
