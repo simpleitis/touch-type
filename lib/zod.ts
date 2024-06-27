a019f3efe1e9623afe7eaf02e44365ba3e0a4306
Amar Krishna <amarkrishna25@gmail.com>
2024-06-27 12:21:40 +0530

    add in signup schema for zod

--- lib/zod.ts
+++ lib/zod.ts
@@ -9,3 +9,16 @@ export const signInSchema = object({
     .min(8, "Password must be more than 8 characters")
     .max(32, "Password must be less than 32 characters"),
 });
+
+export const signUpSchema = object({
+  email: string({ required_error: "Email is required" })
+    .min(1, "Email is required")
+    .email("Invalid email"),
+  password: string({ required_error: "Password is required" })
+    .min(1, "Password is required")
+    .min(8, "Password must be more than 8 characters")
+    .max(32, "Password must be less than 32 characters"),
+  name: string({ required_error: "Email is required" })
+    .min(2, "Name is required")
+    .max(32, "Name must be less than 32 characters"),
+});
