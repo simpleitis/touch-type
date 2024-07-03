8c5b17468242449c42944a351111ccad07f8f3c0
Amar Krishna <amarkrishna25@gmail.com>
2024-07-03 12:55:55 +0530

    add button component

---
+++ app/components/Button.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Button = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <button
+      type="submit"
+      className="mt-4 flex h-10 w-36 items-center justify-center rounded border-2 border-white"
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
