22ff6f55161b213ef29f8638f23304364b55e0ec
Amar Krishna <amarkrishna25@gmail.com>
2024-07-06 17:56:58 +0530

    modify button

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -4,7 +4,7 @@ export default function Button({ children }: { children: React.ReactNode }) {
   return (
     <button
       type="submit"
-      className="mt-4 flex h-10 w-36 items-center justify-center rounded border-2 border-white"
+      className="mt-4 flex h-10 w-36 items-center justify-center rounded-md border-2 border-white"
     >
       {children}
     </button>
