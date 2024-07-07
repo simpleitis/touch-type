55d7a671459211dfad76ead68e7e5313eccf7fe5
Amar Krishna <amarkrishna25@gmail.com>
2024-07-07 20:23:41 +0530

    relocate providers

--- auth.ts
+++ auth.ts
@@ -2,7 +2,9 @@ import NextAuth from "next-auth";
 
 import { prisma } from "./utils/db";
 import { PrismaAdapter } from "@auth/prisma-adapter";
-import authConfig from "./auth.config";
+import Credentials from "next-auth/providers/credentials";
+import GitHub from "next-auth/providers/github";
+import Nodemailer from "next-auth/providers/nodemailer";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
@@ -49,7 +51,27 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       return session;
     },
   },
-  ...authConfig,
+  providers: [
+    Credentials({
+      credentials: {
+        email: {},
+        id: {},
+        name: {},
+      },
+      authorize: async (credentials) => {
+        const email = credentials.email as string;
+        const id = credentials.id as string;
+        const name = credentials.name as string;
+
+        return { email, id, name };
+      },
+    }),
+    GitHub,
+    Nodemailer({
+      server: process.env.EMAIL_SERVER,
+      from: process.env.EMAIL_FROM,
+    }),
+  ],
   pages: {
     signIn: "/login",
   },
