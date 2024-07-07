59caf58617e6c76a81cc1bce385bb07c26c6e748
Amar Krishna <amarkrishna25@gmail.com>
2024-07-07 20:23:25 +0530

    modify auth.config.ts to make it work with edge runtime

--- auth.config.ts
+++ auth.config.ts
@@ -1,28 +1,6 @@
-import Credentials from "next-auth/providers/credentials";
-import GitHub from "next-auth/providers/github";
-import Nodemailer from "next-auth/providers/nodemailer";
 import type { NextAuthConfig } from "next-auth";
 
 export default {
-  providers: [
-    Credentials({
-      credentials: {
-        email: {},
-        id: {},
-        name: {},
-      },
-      authorize: async (credentials) => {
-        const email = credentials.email as string;
-        const id = credentials.id as string;
-        const name = credentials.name as string;
-
-        return { email, id, name };
-      },
-    }),
-    GitHub,
-    Nodemailer({
-      server: process.env.EMAIL_SERVER,
-      from: process.env.EMAIL_FROM,
-    }),
-  ],
+  session: { strategy: "jwt" },
+  providers: [],
 } satisfies NextAuthConfig;
