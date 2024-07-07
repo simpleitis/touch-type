046c0d76d3494013ecde8e010f83164ca3bef147
Amar Krishna <amarkrishna25@gmail.com>
2024-07-07 21:22:39 +0530

    enable auto linking

--- auth.ts
+++ auth.ts
@@ -10,36 +10,6 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
   session: { strategy: "jwt" },
   callbacks: {
-    async signIn({ user, account, profile }) {
-      if (account?.provider === "github") {
-        const email = user.email as string;
-        const name = user.name as string;
-
-        const registeredUser = await prisma.user.findUnique({
-          where: {
-            email: email,
-          },
-        });
-
-        if (!registeredUser?.id) {
-          try {
-            const dbRes = await prisma.user.create({
-              data: {
-                name: name,
-                email: email,
-              },
-            });
-
-            if (dbRes.id) {
-              return true;
-            }
-          } catch (error) {
-            console.log(error);
-          }
-        }
-      }
-      return true;
-    },
     jwt({ token, user }) {
       if (user) {
         token.id = user.id;
@@ -66,7 +36,9 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         return { email, id, name };
       },
     }),
-    GitHub,
+    GitHub({
+      allowDangerousEmailAccountLinking: true,
+    }),
     Nodemailer({
       server: process.env.EMAIL_SERVER,
       from: process.env.EMAIL_FROM,
