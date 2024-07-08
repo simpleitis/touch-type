e61a8ce84d88be6cfcb3d303f9440988acf9dfb1
Amar Krishna <amarkrishna25@gmail.com>
2024-07-08 21:32:30 +0530

    refactor

--- lib/zod.ts
+++ lib/zod.ts
@@ -32,8 +32,15 @@ export const magicLinkSignUpSchema = object({
     .email("Invalid email"),
 });
 
-export const magicLinkSchema = object({
+export const emailSchema = object({
   parsedEmail: string({ required_error: "Email is required" })
     .min(1, "Email is required")
     .email("Invalid email"),
+});
+
+export const passwordSchema = object({
+  parsedPassword: string({ required_error: "Password is required" })
+    .min(1, "Password is required")
+    .min(8, "Password must be more than 8 characters")
+    .max(32, "Password must be less than 32 characters"),
 });
