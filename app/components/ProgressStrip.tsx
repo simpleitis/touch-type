56a58e176587816d4070184301f78d245948c2fc
Amar Krishna <amarkrishna25@gmail.com>
2024-07-14 19:28:55 +0530

    add key

--- app/components/ProgressStrip.tsx
+++ app/components/ProgressStrip.tsx
@@ -5,7 +5,7 @@ export default function ProgressStrip() {
   return (
     <div className="flex gap-2 w-[60%] justify-center mt-10 flex-wrap">
       {progressKeys.map((item) => {
-        return <div className="h-10 w-10 border-4 rounded-full flex justify-center items-center">{item}</div>;
+        return <div key={item} className="h-10 w-10 border-4 rounded-full flex justify-center items-center">{item}</div>;
       })}
     </div>
   );
