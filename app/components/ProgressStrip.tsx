e9f6cc7eba7f81bd6c6d4bfa4359ea7a311a1075
Amar Krishna <amarkrishna25@gmail.com>
2024-07-14 21:01:52 +0530

    add color coding in progress strip

--- app/components/ProgressStrip.tsx
+++ app/components/ProgressStrip.tsx
@@ -1,12 +1,42 @@
 import React from "react";
 import { progressKeys } from "../helpers/keyboard";
+import { auth } from "@/auth";
+import { getUserInfo } from "../actions/userInfo";
 
-export default function ProgressStrip() {
-  return (
-    <div className="flex gap-2 w-[60%] justify-center mt-10 flex-wrap">
-      {progressKeys.map((item) => {
-        return <div key={item} className="h-10 w-10 border-4 rounded-full flex justify-center items-center">{item}</div>;
-      })}
-    </div>
-  );
+export default async function ProgressStrip() {
+  const session = await auth();
+
+  const id = session?.user?.id as string;
+
+  const userInfoRes = await getUserInfo(id);
+
+  if (userInfoRes?.success && userInfoRes.userInfo?.progress) {
+    return (
+      <div className="mt-10 flex w-[60%] flex-wrap justify-center gap-2 font-semibold text-xl">
+        {progressKeys.map((item, index) => {
+          if (index < userInfoRes.userInfo?.progress) {
+            return (
+              <div
+                key={item}
+                className="flex h-12 w-12 items-center justify-center rounded-full border-4 border-lime-400"
+              >
+                {item}
+              </div>
+            );
+          } else {
+            return (
+              <div
+                key={item}
+                className="flex h-12 w-12 items-center justify-center rounded-full border-4 border-slate-800 text-slate-500"
+              >
+                {item}
+              </div>
+            );
+          }
+        })}
+      </div>
+    );
+  } else if (!userInfoRes?.success) {
+    return <p>Error loading progress!</p>;
+  }
 }
