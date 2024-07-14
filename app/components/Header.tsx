6d47871581be63998d31ccbc2ef9c13a5eff4c87
Amar Krishna <amarkrishna25@gmail.com>
2024-07-14 13:21:19 +0530

    fix header styling

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,8 +3,8 @@ import LogoutButton from "./LogoutButton";
 
 export default function Header() {
   return (
-    <div className="flex h-screen w-screen flex-col items-center">
-      <div className="mt-5 flex h-20 w-[98%] items-center justify-between rounded-lg border border-white border-opacity-20 bg-white bg-opacity-10 p-6 backdrop-blur-lg">
+    <div className="flex w-full flex-col items-center">
+      <div className="flex h-20 w-full items-center justify-between rounded-lg border border-white border-opacity-20 bg-white bg-opacity-10 p-6 backdrop-blur-lg">
         <p className="text-4xl font-bold">touchtype_</p>
         <LogoutButton />
       </div>
