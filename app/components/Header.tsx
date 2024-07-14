ce18c7b807065cc5a61da53ad7d156850605724d
Amar Krishna <amarkrishna25@gmail.com>
2024-07-14 12:43:31 +0530

    implement logout button

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import React from "react";
+import LogoutButton from "./LogoutButton";
 
 export default function Header() {
   return (
-    <div>Header</div>
-  )
+    <div className="flex h-screen w-screen flex-col items-center">
+      <div className="mt-5 flex h-20 w-[98%] items-center justify-between rounded-lg border border-white border-opacity-20 bg-white bg-opacity-10 p-6 backdrop-blur-lg">
+        <p className="text-4xl font-bold">touchtype_</p>
+        <LogoutButton />
+      </div>
+    </div>
+  );
 }
---
+++ app/components/LogoutButton.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import React from "react";
+import { IoPower } from "react-icons/io5";
+import { signOut } from "next-auth/react";
+
+export default function LogoutButton() {
+  return (
+    <button
+      className="flex items-center gap-2 p-2 text-lg text-red-500"
+      onClick={() => signOut()}
+    >
+      <IoPower />
+      Logout
+    </button>
+  );
+}
