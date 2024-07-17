62f0685b2c370bbb716f31845966fa5f5c9b25bf
Amar Krishna <amarkrishna25@gmail.com>
2024-07-17 10:23:05 +0530

    adjust header css

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -4,8 +4,8 @@ import LogoutButton from "./LogoutButton";
 export default function Header() {
   return (
     <div className="flex w-full flex-col items-center">
-      <div className="flex h-20 w-full items-center justify-between rounded-lg border border-white border-opacity-20 bg-white bg-opacity-10 p-6 backdrop-blur-lg">
-        <p className="text-4xl font-bold">touchtype_</p>
+      <div className="flex h-20 w-full items-center justify-between rounded-lg border-4 border-white p-6 backdrop-blur-lg">
+        <p className="text-4xl font-extrabold">touchtype_</p>
         <LogoutButton />
       </div>
     </div>
--- app/components/LogoutButton.tsx
+++ app/components/LogoutButton.tsx
@@ -7,7 +7,7 @@ import { signOut } from "next-auth/react";
 export default function LogoutButton() {
   return (
     <button
-      className="flex items-center gap-2 p-2 text-lg text-red-500"
+      className="flex items-center gap-2 p-2 text-lg text-red-500 font-medium"
       onClick={() => signOut()}
     >
       <IoPower />
