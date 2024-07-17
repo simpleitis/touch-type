04254b0a6263237ccdec6868a4b0ce3091e3c1b0
Amar Krishna <amarkrishna25@gmail.com>
2024-07-17 12:37:58 +0530

    add hide component to main context

--- app/context/MainContext.tsx
+++ app/context/MainContext.tsx
@@ -3,11 +3,15 @@ import React, { useState } from "react";
 interface MainContextType {
   start: boolean;
   setStart: React.Dispatch<React.SetStateAction<boolean>>;
+  hide: boolean;
+  setHide: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 export const MainContext = React.createContext<MainContextType>({
   start: false,
   setStart: () => {},
+  hide: false,
+  setHide: () => {},
 });
 
 export default function MainContextProvider({
@@ -16,9 +20,10 @@ export default function MainContextProvider({
   children: React.ReactNode;
 }) {
   const [start, setStart] = useState(false);
+  const [hide, setHide] = useState(false);
 
   return (
-    <MainContext.Provider value={{ start, setStart }}>
+    <MainContext.Provider value={{ start, setStart, hide, setHide }}>
       {children}
     </MainContext.Provider>
   );
