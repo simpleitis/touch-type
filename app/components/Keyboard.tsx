ff7618936dba5700f14dbe8fc49ce4101412b27d
Amar Krishna <amarkrishna25@gmail.com>
2024-07-17 10:07:45 +0530

    adjust keyboard size

--- app/components/Keyboard.tsx
+++ app/components/Keyboard.tsx
@@ -1,11 +1,14 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { keys, specialKeys } from "../helpers/keyboard";
 import Key from "./Key";
+import { MainContext } from "../context/MainContext";
 
 const Keyboard = () => {
   const [pressedKey, setPressedKey] = useState<string | null>(null);
+
+  const { start, setStart } = useContext(MainContext);
 
   const handleKeyDown = (event: KeyboardEvent) => {
     setPressedKey(event.code);
@@ -23,20 +26,30 @@ const Keyboard = () => {
   }, []);
 
   return (
-    <div className="w-full">
-      {keys.map((row, rowIndex) => (
-        <div key={rowIndex} className="mb-1 flex items-center justify-center">
-          {row.map((item) => (
-            <Key
-              value={item.value}
-              display={item.display}
-              key={item.value}
-              isPressed={pressedKey === item.value}
-              specialWidth={specialKeys[item.display]}
-            />
-          ))}
+    <div className="relative flex h-[540px] w-[1200px] items-center justify-center">
+      {!start && (
+        <div
+          className="w-100% h-100% absolute bottom-0 left-0 right-0 top-0 z-10 flex cursor-pointer items-center justify-center rounded-xl bg-white bg-opacity-10 backdrop-blur-lg"
+          onClick={() => setStart(true)}
+        >
+          <p className="text-2xl text-slate-200">Click to start</p>
         </div>
-      ))}
+      )}
+      <div>
+        {keys.map((row, rowIndex) => (
+          <div key={rowIndex} className="mb-1 flex items-center justify-center">
+            {row.map((item) => (
+              <Key
+                value={item.value}
+                display={item.display}
+                key={item.value}
+                isPressed={pressedKey === item.value}
+                specialWidth={specialKeys[item.display]}
+              />
+            ))}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
