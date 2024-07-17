79a348bf41011a73cb93fc68f5c2c722110b143d
Amar Krishna <amarkrishna25@gmail.com>
2024-07-17 10:34:03 +0530

    fix header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -4,7 +4,7 @@ import LogoutButton from "./LogoutButton";
 export default function Header() {
   return (
     <div className="flex w-full flex-col items-center">
-      <div className="flex h-20 w-full items-center justify-between rounded-lg border-4 border-white p-6 backdrop-blur-lg">
+      <div className="flex mt-2 h-20 w-full items-center justify-between border-4 border-white p-6 backdrop-blur-lg rounded-full">
         <p className="text-4xl font-extrabold">touchtype_</p>
         <LogoutButton />
       </div>
--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import ProgressStrip from "./components/ProgressStrip";
 export default function Home() {
   return (
     <MainContextWrapper>
-      <div className="h-screen px-10 pt-5">
+      <div className="h-screen px-5">
         <Header />
         <div className="flex flex-col items-center">
           <ProgressStrip />
