97a873452ee1659bcc2ad9210094a9f30c8b777d
Amar Krishna <amarkrishna25@gmail.com>
2024-07-25 12:09:57 +0530

    add color option for keycomponent

--- app/components/Key.tsx
+++ app/components/Key.tsx
@@ -5,12 +5,13 @@ interface KeyProps {
   display: string;
   isPressed: boolean;
   specialWidth: string;
+  colored?: boolean;
 }
 
-function Key({ display, isPressed, specialWidth }: KeyProps) {
+function Key({ display, isPressed, specialWidth, colored }: KeyProps) {
   return (
     <div
-      className={`m-2 flex h-16 items-center justify-center rounded-full border-2 border-white border-opacity-40 px-2 py-1 shadow-lg ${
+      className={`m-2 flex h-16 items-center justify-center rounded-full border-2 ${colored ? "border-4 border-lime-600" : "border-white border-opacity-40"} px-2 py-1 shadow-lg ${
         specialWidth || "w-16"
       } ${isPressed ? "bg-white text-black" : "bg-transparent text-white"}`}
     >
